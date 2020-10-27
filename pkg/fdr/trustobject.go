6eb8300755c0b3cf2e7234db7603948678aafb72
Rick Mark <rickmark@outlook.com>
2020-10-26 21:41:21 -0700

    Test data

--- pkg/fdr/trustobject.go
+++ pkg/fdr/trustobject.go
@@ -1,2 +1,20 @@
 package fdr
 
+import "crypto/x509"
+
+const (
+	TrustObjectMagic = "secb"
+	TrustObjectSigning = "trst"
+	TrustObjectTransport = "rssl"
+	TrustObjectRevocation = "rvok"
+)
+
+type TrustObject struct {
+	SigningCertificate *x509.Certificate
+	TransportCertificate *x509.Certificate
+	Revocation *x509.RevocationList
+}
+
+func parseTrustObject(data []byte) (*TrustObject, error) {
+
+}
